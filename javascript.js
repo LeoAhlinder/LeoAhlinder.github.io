6065a49f742c894c3864318b321eb4baa15d7913
Leo Ahlinder <ahlinder.leo@icloud.com>
2023-08-24 15:58:30 +0200

    yo

--- javascript.js
+++ javascript.js
@@ -1,3 +1,14 @@
-function ThemeChange(){
-    console.log("2")
+function selectedProject(project){
+
+    const pindeCode = document.getElementById("pincodeProject");
+    const lightnovelProject = document.getElementById("lightnovelProject")
+
+    pindeCode.classList.remove("selected")
+    lightnovelProject.classList.remove("selected")
+
+
+    const element = document.getElementById(project.id)
+    element.classList.add("selected")
+
+
 }
