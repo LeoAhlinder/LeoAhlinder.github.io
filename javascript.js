1998bd89b6ac4833790826eb38a85c7f90c1e514
Leo Ahlinder <ahlinder.leo@icloud.com>
2023-08-25 09:20:40 +0200

    updates

--- javascript.js
+++ javascript.js
@@ -1,14 +1,28 @@
 function selectedProject(project){
 
+    //Add new element here and in the array to make it work.
+
     const pindeCode = document.getElementById("pincodeProject");
     const lightnovelProject = document.getElementById("lightnovelProject")
 
-    pindeCode.classList.remove("selected")
-    lightnovelProject.classList.remove("selected")
+    const elements = [pindeCode,lightnovelProject]
+
+    for (let i = 0;i<elements.length;i++){
+        if (elements[i] === project){
+            if (project.classList.contains("selected")){
+                project.classList.remove("selected")
+            }
+            else{
+                project.classList.add("selected")
+            }
+        }
+        else{
+            elements[i].classList.remove("selected")
+        }
+    }
+}
 
 
-    const element = document.getElementById(project.id)
-    element.classList.add("selected")
-
+function changeTheme(){
 
 }
