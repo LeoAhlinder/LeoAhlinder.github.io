576296d008cdc7ccfaa79cf594be39ac7b9412c2
Leo Ahlinder <ahlinder.leo@icloud.com>
2023-09-01 15:17:41 +0200

    Better responsive and fixed the project selection

--- javascript.js
+++ javascript.js
@@ -1,84 +1,101 @@
-function selectedProject(project){
-
-    //Add new element here and in the array to make it work.
-
-    const pindeCode = document.getElementById("pincodeProject");
-    const lightnovelProject = document.getElementById("lightnovelProject")
+let lastClickTime = 0;
 
 
-    const projectDescriptionText = ["Web-based employee management system that allows employees to record their work hours, clock in and out, track break times using a 4-digit pin code, and view their calculated pay. The system is managed by an admin who can add new employees, set their pay rates, and make adjustments as needed."
-    ,"TALWDWOAM"]
-
-    const gitHubLinks = ["https://github.com/LeoAhlinder/pincode","https://github.com/LeoAhlinder/LightNovelOnline"]
-
-    const elements = [pindeCode,lightnovelProject]
+function selectedProject(project) {
 
 
-    if (project === 0){
-        for (let i = 0;i<elements.length;i++){
-            elements[i].classList.remove("selected")
-            elements[i].style.opacity = 1;
-            const githubIcons = document.getElementsByClassName("fa");
-            for (const githubIcon of githubIcons) {
-                document.getElementById("projectBox").removeChild(githubIcon)
-            }
-
-        }
-        document.getElementById("projectName").innerHTML = "";
-        document.getElementById("projectDescription").innerHTML = "";
-
-        return
+	const currentTime = Date.now();
+    if (currentTime - lastClickTime < 800 ) {
+        return; 
     }
-    
-        for (let i = 0;i<elements.length;i++){
-        if (elements[i] === project){
-            if (project.classList.contains("selected")){
-                project.classList.remove("selected")
-                document.getElementById("projectName").innerHTML = "";
-                document.getElementById("projectDescription").innerHTML = "";
-                project.style.opacity = 1;
-                const githubIcons = document.getElementsByClassName("fa");
-                for (const githubIcon of githubIcons) {
-                    document.getElementById("projectBox").removeChild(githubIcon)
-                }
+	if (project != 0){
+    	lastClickTime = currentTime;
+	}
+	const pindeCode = document.getElementById("pincodeProject");
+	const lightnovelProject = document.getElementById("lightnovelProject");
 
-            }
-            else{
-                project.classList.add("selected");
-                document.getElementById("projectName").innerHTML = "";
-                document.getElementById("projectDescription").innerHTML = "";
-                project.style.opacity = 0;
-               
+	const projectDescriptionText = [
+		"Web-based employee management system that allows employees to record their work hours, clock in and out, track break times using a 4-digit pin code, and view their calculated pay. The system is managed by an admin who can add new employees, set their pay rates, and make adjustments as needed.",
+		"TALWDWOAM",
+	];
 
+	const gitHubLinks = [
+		"https://github.com/LeoAhlinder/pincode",
+		"https://github.com/LeoAhlinder/LightNovelOnline",
+	];
 
-                setTimeout(function(){
-                    document.getElementById("projectName").innerHTML = project.innerHTML;
-                },500)
-                setTimeout(function(){
-                    document.getElementById("projectDescription").innerHTML = projectDescriptionText[i];
-                    const li = document.createElement("li");
-                    li.classList.add("fa", "fa-github", "githubIcon");
-                    li.addEventListener("click",function(){
-                        window.location = gitHubLinks[i]
-                    })
+	const elements = [pindeCode, lightnovelProject];
 
-                    document.getElementById("projectBox").append(li)
-                },1000)
-            }
-        }
-        else{
-            elements[i].classList.remove("selected")
-            elements[i].style.opacity = 1;
-            const githubIcons = document.getElementsByClassName("fa");
-            for (const githubIcon of githubIcons) {
-                document.getElementById("projectBox").removeChild(githubIcon)
-            }
-            
-        }
-    }
+	if (project === 0) {
+		for (let i = 0; i < elements.length; i++) {
+			elements[i].classList.remove("selected");
+			elements[i].style.opacity = 1;
+			const githubIcons = document.getElementsByClassName("fa");
+			for (const githubIcon of githubIcons) {
+				document.getElementById("projectBox").removeChild(githubIcon);
+			}
+		}
+
+		document.getElementById("projectName").innerHTML = "";
+		document.getElementById("projectDescription").innerHTML = "";
+
+		return;
+	}
+
+	for (let i = 0; i < elements.length; i++) {
+		if (elements[i] === project) {
+			if (project.classList.contains("selected")) {
+				project.classList.remove("selected");
+				document.getElementById("projectName").innerHTML = "";
+				document.getElementById("projectDescription").innerHTML = "";
+				project.style.opacity = 1;
+				const githubIcons = document.getElementsByClassName("fa");
+				for (const githubIcon of githubIcons) {
+					document.getElementById("projectBox").removeChild(githubIcon);
+				}
+			} else {
+				project.classList.add("selected");
+				document.getElementById("projectName").innerHTML = "";
+				document.getElementById("projectDescription").innerHTML = "";
+				project.style.opacity = 0;
+
+				setTimeout(function () {
+					document.getElementById("projectName").innerHTML = project.innerHTML;
+				}, 500);
+				setTimeout(function () {
+					document.getElementById("projectDescription").innerHTML =
+						projectDescriptionText[i];
+					const li = document.createElement("li");
+					li.classList.add("fa", "fa-github", "githubIcon");
+					li.addEventListener("click", function () {
+						window.location = gitHubLinks[i];
+					});
+
+					document.getElementById("projectBox").append(li);
+				}, 700);
+			}
+		} else {	
+			elements[i].classList.remove("selected");
+			elements[i].style.opacity = 1;
+			const githubIcons = document.getElementsByClassName("fa");
+			for (const githubIcon of githubIcons) {
+				document.getElementById("projectBox").removeChild(githubIcon);
+			}
+		}
+	}
 }
 
-
-function changeTheme(){
-
+function changeTheme() {
+	const element = document.querySelector("html");
+	if (element.getAttribute("id") === "darkMode") {
+		element.removeAttribute("data-bs-theme");
+		element.removeAttribute("id", "darkMode");
+		element.setAttribute("id", "light");
+		element.setAttribute("data-bs-theme", "light");
+	} else {
+		element.removeAttribute("data-bs-theme");
+		element.removeAttribute("id", "lightMode");
+		element.setAttribute("data-bs-theme", "dark");
+		element.setAttribute("id", "darkMode");
+	}
 }
