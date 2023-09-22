9ac2cb639511df1f4b245ecd5c20dab066a27f52
Leo Ahlinder <ahlinder.leo@icloud.com>
2023-09-22 15:36:31 +0200

    added background image and project img

--- javascript.js
+++ javascript.js
@@ -1,120 +1,33 @@
-let lastClickTime = 0;
+function selectedProject(projectPressed) {
+  console.log(projectPressed);
 
-function selectedProject(project) {
-  const currentTime = Date.now();
-  if (currentTime - lastClickTime < 800) {
-    return;
-  }
-  if (project != 0) {
-    lastClickTime = currentTime;
-  }
-  const pindeCode = document.getElementById("pincodeProject");
-  const lightnovelProject = document.getElementById("lightnovelProject");
+  const employee = document.getElementById("employee");
+  const library = document.getElementById("library");
   const carRental = document.getElementById("carRental");
-
-  const projectDescription = document.getElementById("projectDescription");
-  const projectBox = document.getElementById("projectBox");
-  const projectName = document.getElementById("projectName");
   //Projecttext,livedemo and github links are the same order as these are made
 
   const projectDescriptionText = [
-    "Web-based employee management system that allows employees to record their work hours, clock in and out, track break times using a 4-digit pin code, and view their calculated pay. The system is managed by an admin who can add new employees, set their pay rates, and make adjustments as needed.",
-    "Books online",
     "Car rental service",
-    "this is me",
+    "Books online",
+    "Web-based employee management system that allows employees to record their work hours, clock in and out, track break times using a 4-digit pin code, and view their calculated pay. The system is managed by an admin who can add new employees, set their pay rates, and make adjustments as needed.",
   ];
 
-  const liveDemo = [
-    "",
-    "",
-    "https://ntig-uppsala.github.io/ntbbiluthyrning/",
-    "",
-  ];
+  const liveDemo = ["https://ntig-uppsala.github.io/ntbbiluthyrning/", "", ""];
 
   const gitHubLinks = [
-    "https://github.com/LeoAhlinder/pincode",
-    "https://github.com/LeoAhlinder/LightNovelOnline",
     "https://github.com/NTIG-Uppsala/ntbbiluthyrning",
-    "",
+    "https://github.com/LeoAhlinder/LightNovelOnline",
+    "https://github.com/LeoAhlinder/pincode",
   ];
 
-  const elements = [pindeCode, lightnovelProject, carRental];
+  const projects = [carRental, library, employee];
 
-  if (project === 0) {
-    for (let i = 0; i < elements.length; i++) {
-      elements[i].classList.remove("selected");
-      elements[i].style.opacity = 1;
-      const githubIcons = document.getElementsByClassName("fa");
-      projectBox.classList.add("containerL");
-      projectBox.classList.remove("containerLUsed");
-      for (const githubIcon of githubIcons) {
-        projectBox.removeChild(githubIcon);
-      }
-    }
-
-    projectName.innerHTML = "";
-    projectDescription.innerHTML = "";
-    return;
-  }
-
-  for (let i = 0; i < elements.length; i++) {
-    if (elements[i] === project) {
-      if (project.classList.contains("selected")) {
-        project.classList.remove("selected");
-        projectName.innerHTML = "";
-        projectDescription.innerHTML = "";
-        projectBox.classList.add("containerL");
-        projectBox.classList.remove("containerLUsed");
-        project.style.opacity = 1;
-        const githubIcons = document.getElementsByClassName("fa");
-        for (const githubIcon of githubIcons) {
-          projectBox.removeChild(githubIcon);
-        }
-      } else {
-        project.classList.add("selected");
-        projectName.innerHTML = "";
-        projectDescription.innerHTML = "";
-        project.style.opacity = 0;
-        setTimeout(function () {
-          projectBox.classList.remove("containerL");
-          projectBox.classList.add("containerLUsed");
-          projectName.innerHTML = project.innerHTML;
-        }, 700);
-        setTimeout(function () {
-          let projectDescriptionIndex = 0;
-          let projectText = "";
-          function typeProjectName() {
-            if (projectDescriptionIndex < projectDescriptionText[i].length) {
-              projectText += projectDescriptionText[i].charAt(
-                projectDescriptionIndex
-              );
-              projectDescription.innerText = projectText;
-              projectDescriptionIndex++;
-              setTimeout(typeProjectName, 15);
-            }
-          }
-          typeProjectName();
-
-          if (gitHubLinks[i] !== "") {
-            const li = document.createElement("li");
-            li.classList.add("fa", "fa-github", "githubIcon");
-            li.setAttribute("target", "_blank");
-            li.addEventListener("click", function () {
-              window.open(gitHubLinks[i], "_blank");
-            });
-            projectBox.append(li);
-          }
-        }, 1000);
-      }
+  for (const project of projects) {
+    if (project === projectPressed) {
+      project.classList.add("selectedProject");
+      console.log(project.classList);
     } else {
-      elements[i].classList.remove("selected");
-      elements[i].style.opacity = 1;
-      const githubIcons = document.getElementsByClassName("fa");
-      for (const githubIcon of githubIcons) {
-        projectBox.removeChild(githubIcon);
-      }
-      projectBox.classList.add("containerL");
-      projectBox.classList.remove("containerLUsed");
+      project.style.display = "none";
     }
   }
 }
@@ -124,16 +37,21 @@ function changeTheme() {
   const moon = document.querySelector(".moon");
   sun.classList.toggle("visible");
   moon.classList.toggle("visible");
+
+  const background = document.getElementById("homePage");
+
   const element = document.querySelector("html");
   if (element.getAttribute("id") === "darkMode") {
     element.removeAttribute("data-bs-theme");
     element.removeAttribute("id", "darkMode");
     element.setAttribute("id", "light");
     element.setAttribute("data-bs-theme", "light");
+    background.style.backgroundImage = "url('pictures/sunrisebackground.png')";
   } else {
     element.removeAttribute("data-bs-theme");
     element.removeAttribute("id", "lightMode");
     element.setAttribute("data-bs-theme", "dark");
     element.setAttribute("id", "darkMode");
+    background.style.backgroundImage = "url('pictures/shootingstar.jpg')";
   }
 }
