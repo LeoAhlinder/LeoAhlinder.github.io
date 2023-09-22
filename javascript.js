06f9641d38cab1917fdf64385899ebc358b33c2e
xxudon <ahlinder.leo@icloud.com>
2023-09-22 18:58:36 +0200

    added barbershop project

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ function selectedProject(projectPressed) {
   const employee = document.getElementById("employee");
   const library = document.getElementById("library");
   const carRental = document.getElementById("carRental");
+  const barberShop = document.getElementById("barberShop")
   
   const projectTitle = document.getElementById("projectTitle")
   //Projecttext,livedemo and github links are the same order as these are made
@@ -12,30 +13,29 @@ function selectedProject(projectPressed) {
     "Car rental service",
     "Books online",
     "Web-based employee management system that allows employees to record their work hours, clock in and out, track break times using a 4-digit pin code, and view their calculated pay. The system is managed by an admin who can add new employees, set their pay rates, and make adjustments as needed.",
+    "Barber Shop"
   ];
 
-  const liveDemo = ["https://ntig-uppsala.github.io/ntbbiluthyrning/", "", ""];
+  const liveDemo = ["https://ntig-uppsala.github.io/ntbbiluthyrning/", "", "","https://ntig-uppsala.github.io/frisorsaxe/"];
 
   const gitHubLinks = [
     "https://github.com/NTIG-Uppsala/ntbbiluthyrning",
     "https://github.com/LeoAhlinder/LightNovelOnline",
     "https://github.com/LeoAhlinder/pincode",
+    "https://github.com/NTIG-Uppsala/frisorsaxe"
   ];
 
-  const projects = [carRental, library, employee];
+  const projects = [carRental, library, employee,barberShop];
 
-  const projectsTitle = ["Car Rental", "Library Online","Emplyoee Pay Tracker"]
-  let i = 0;
 
   for (const project of projects) {
     if (project === projectPressed) {
       project.classList.add("selectedProject");
       console.log(project.classList);
-      projectTitle.innerText = projectsTitle[i];
+      projectTitle.innerText = "";
     } else {
       project.style.display = "none";
     }
-    i++
   }
 }
 
